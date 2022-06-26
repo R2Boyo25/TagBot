0cfd7733a8714eaecf2647fbadf6da78979d7e80
R2boyo25 <r2boyo25@gmail.com>
2022-06-26 14:22:17 -0500

    And more merge conflicts :)

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,8 +1,5 @@
 import discord
-<<<<<<< HEAD
 import os
-=======
->>>>>>> e50a8c1 (fix merge conflict)
 from discord.ext import commands
 from utils.funcs import *
 from database import Database as db
